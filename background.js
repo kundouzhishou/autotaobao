1a02fe9e43aad0e9cc69dd49a8efcb0d3e4e8b8e
kundouzhishou <kundouzhishou@gmail.com>
2013-08-20 12:58:30 +0800

    autotaobao first commit

---
+++ background.js
@@ -0,0 +1,35 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	url = "https://login.taobao.com/member/login.jhtml";
+	alert(11);
+	chrome.tabs.create( { url:url }, function(){
+		login();
+	});
+});
+
+function login() {
+	chrome.tabs.executeScript(null,{file:"login.js"},function(){
+		//chrome.tabs.onUpdated.addListener(updateTab);
+		window.open("https://login.taobao.com/member/login.jhtml");
+	});
+}
+
+function updateTab( tabId, info, tab ) {	
+	//alert("update tab");
+	chrome.tabs.getSelected(null, function(tab) {
+		if( info.status=="complete" ){ 
+			//alert("update complete");
+			chrome.tabs.onUpdated.removeListener(updateTab);
+		}
+    });
+}
+
+function getValue(key) {
+	return localStorage.getItem(key);
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+    if (request.type == 'localStorage'){
+		//alert("save -- username:" + request.name + ",password:" + request.value);
+        localStorage.setItem(request.name, request.value);
+    }
+});
---
+++ login.js
@@ -0,0 +1,4 @@
+//alert("to login...");
+document.getElementById('TPL_username_1').value="jinsuiyong";
+document.getElementById('TPL_password_1').value="jsyjsy19861108";
+document.getElementById('J_SubmitStatic').click();
