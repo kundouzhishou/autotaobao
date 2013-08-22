6468f508d093e1ef4f68640bf1708d3c43b8d3d1
kundouzhishou <kundouzhishou@gmail.com>
2013-08-23 01:55:03 +0800

    delete js

--- contentscripts.js
+++
@@ -1,1 +0,0 @@
-alert("empty contenst scirpt");chrome.extension.sendRequest({url:"https://login.taobao.com/member/login.jhtml"}, function(response) {	alert("send request");	$(".TPL_username_1 input").val("jinsuiyong");	$(".TPL_password_1 input").val("jsyjsy19861108");	//$("#J_SubmitStatic").click();});
