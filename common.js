b7e78bc4b6eb2230d2ef10e02c6166f2b9a66997
kundouzhishou <kundouzhishou@gmail.com>
2013-09-01 19:59:15 +0800

    delete common.js

--- common.js
+++
@@ -1,9 +0,0 @@
-function getElementByTagAndAttribute(element,tagname,attname,attvalue) {
-    var elems = element.getElementsByTagName(tagname), i;
-    for (i in elems) {
-		if(elems[i].getAttribute && elems[i].getAttribute(attname) && elems[i].getAttribute(attname).indexOf(attvalue) != -1) {
-			return elems[i];
-		}
-    }
-	return undefined;
-}
